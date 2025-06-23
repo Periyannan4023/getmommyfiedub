f89d10fc6fcf3c75020c40180ff1f73157c5e851
Sathish <112696144+Periyannan4023@users.noreply.github.com>
2025-06-23 22:18:47 +0530

    Add files via upload

---
+++ asstes/main.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+        }
+      });
+    }, { threshold: 0.2 });
+
+    document.querySelectorAll('.fade-up, .fade-right, .fade-left').forEach(el => {
+      observer.observe(el);
+    });
+  });
+
+  //carousel////
